84a26eafd5a9a61cb1b4ade0cd7a64fb069a1653
Lovsic97 <lj1831754475@163.com>
2022-09-28 04:07:07 +0100

    首页新闻列表完成

--- src/main.js
+++ src/main.js
@@ -3,7 +3,7 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 /* 导入vant */
-import Vant from 'vant'
+import Vant, { Lazyload } from 'vant'
 import './assets/reset.min.css'
 import 'amfe-flexible'
 import 'vant/lib/index.css'
@@ -13,4 +13,8 @@ handleMaxWidth()
 window.addEventListener('resize', handleMaxWidth)
 
 const app = createApp(App)
-app.use(Vant).use(store).use(router).mount('#app')
+app.use(Vant)
+app.use(Lazyload, {
+  lazyComponent: true
+})
+app.use(store).use(router).mount('#app')
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -15,28 +15,46 @@
       </van-swipe-item>
     </van-swipe>
   </section>
+  <!-- 新闻列表 -->
+  <van-skeleton title :row="5" v-if="newsList.length === 0"></van-skeleton>
+  <div v-else>
+    <section class="news-box" v-for="(item, index) in newsList" :key="index">
+      <van-divider content-position="left" v-if="index !== 0">
+        {{formatTime(item.date, '{1}月{2}日')}}
+      </van-divider>
+      <div class="content">
+        <Item v-for="item2 in item.stories" :key="item2.id" :data="item2"></Item>
+      </div>
+    </section>
+  </div>
+  <div class="lazy-more" v-show="newsList.length !== 0" ref="loadMore">
+      <van-loading size="12px">数据正在加载中...</van-loading>
+    </div>
 </template>
 
 <script>
 import Head from '@/components/Head.vue'
+import Item from '@/components/Item.vue'
 import api from '@/api/index.js'
-import { reactive, toRefs } from '@vue/reactivity'
-import { onBeforeMount } from '@vue/runtime-core'
+import { reactive, toRefs, ref } from '@vue/reactivity'
+import { onBeforeMount, onMounted } from '@vue/runtime-core'
+import { formatTime } from '@/assets/utils.js'
 export default {
   name: "Home",
-  components: { Head },
+  components: { Head, Item },
   setup() {
     let state = reactive({
       today: '',
-      newList: [],
+      newsList: [],
       bannerList: []
     })
+    let loadMore = ref(null)
 
     // 第一次加载数据
     onBeforeMount(async () => {
       let {date, stories, top_stories} = await api.queryNewsLatest()
       state.today = date
-      state.newList.push(
+      state.newsList.push(
         Object.freeze({
         date,
         stories
@@ -45,18 +63,39 @@ export default {
       state.bannerList = Object.freeze(top_stories)
     })
 
+    // 第一次渲染完，加载更多数据
+    onMounted(() => {
+      let ob = new IntersectionObserver(async (change) => {
+        let item = change[0]
+        if (item.isIntersecting) {
+          let result = await api.queryNewsBefore(
+            state.newsList[state.newsList.length - 1]["date"]
+          )
+          state.newsList.push(Object.freeze(result))
+        }
+      })
+      ob.observe(loadMore.value)
+    })
+
     return {
-      ...toRefs(state)
+      ...toRefs(state),
+      formatTime,
+      loadMore
     }
   }
 }
 </script>
 
 <style lang="less" scoped>
+.van-skeleton {
+  padding: 30px 15px;
+}
+
 .banner-box {
   box-sizing: border-box;
   height: 375px;
   background-color: #d5d5d5;
+  margin-bottom: 10px;
 
   .van-swipe {
     height: 100%;
@@ -116,5 +155,27 @@ export default {
       }
     }
   }
+}
+
+.news-box {
+  padding: 0 15px;
+
+  .van-divider {
+    margin: 5px 0;
+    font-size: 12px;
+    &:before {
+      display: none;
+    }
+    &:after {
+      border-color: #d9d5d5;
+    }
+  }
+}
+
+.lazy-more {
+  display: flex;
+  justify-content: center;
+  padding: 10px;
+  background: #f4f4f4;
 }
 </style>
